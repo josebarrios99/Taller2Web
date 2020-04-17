d8f491639b86a07f09852f34acbe64184ee7882a
Angela <patataramirez13@gmail.com>
2020-04-17 13:10:21 -0500

    Path to games section

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 
+const path = require('path');
+
+const exphbs = require('express-handlebars');
+
 const app = express();
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-app.get('/', function (request, response) {
+app.get('/home', function (request, response) {
     console.log('hola en la consola');
     response.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+
+  app.get('/store', function (req, res) {
+    res.render('games');
   });
 
 app.listen(3000, function () {
