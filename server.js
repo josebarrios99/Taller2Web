0bb45fe4b95137775e55f73c086c55986948fbb6
Angela <patataramirez13@gmail.com>
2020-04-16 19:44:57 -0500

    Added the homepage

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, function () {
-    console.log('servidor iniciado en puerto 3000');
+    console.log('servidor iniciado');
+  });
+app.get('/', function (request, response) {
+    console.log('hola en la consola');
+    response.sendFile(path.join(__dirname, '/public/index.html'));
   });
