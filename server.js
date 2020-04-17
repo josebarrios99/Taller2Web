a067df6d18dde354a1ab55ba8a6bb14170ab4334
Angela <patataramirez13@gmail.com>
2020-04-17 11:10:05 -0500

    Homepage running

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, function () {
-    console.log('servidor iniciado');
-  });
+app.use(express.static('public'));
+
 app.get('/', function (request, response) {
     console.log('hola en la consola');
     response.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+
+app.listen(3000, function () {
+    console.log('servidor iniciado');
   });
