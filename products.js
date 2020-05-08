4b98232152ac1f6a3700a8993df2ee5d26324555
Angela <patataramirez13@gmail.com>
2020-05-08 17:39:03 -0500

    Added some variables to product section

---
+++ products.js
@@ -0,0 +1,36 @@
+const products = [
+
+    {
+        title: 'Sekiro',
+        description: 'lorem',
+        price: '59USD',
+        img: '/images/game1.png',
+        id: '1',
+      },
+
+    {
+        title: 'Dark souls III',
+        description: '',
+        price: '59USD',
+        img: '/images/game2.png',
+        id: '2',
+    
+      },
+
+    {
+        title: 'Blood borne',
+        description: '',
+        price: '59USD',
+        img: '/images/game3.png',
+        id: '3',
+    
+      },
+
+    {
+        title: 'Demon souls',
+        description: '',
+        price: '29USD',
+        id: '4',
+      },
+];
+module.exports = products;
--- server.js
+++ server.js
@@ -4,19 +4,31 @@ const path = require('path');
 
 const exphbs = require('express-handlebars');
 
+const products = require('./products')
+
 const app = express();
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 app.get('/home', function (request, response) {
-    console.log('hola en la consola');
     response.sendFile(path.join(__dirname, '/public/index.html'));
   });
 
   app.get('/store', function (req, res) {
     res.render('games');
+  });
+  app.get('/product/:name/:id', function (req, res) {
+    var context = {};
+    var foundElement =  products.find(function(elem){
+      if(elem.title == req.params.name){
+        return true;
+      }
+    });
+    context = foundElement;
+    res.render('product', context);
   });
 
 app.listen(3000, function () {
