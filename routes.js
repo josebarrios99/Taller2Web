e7b4219745bacee2d7bdae85a6f4120da1c1cfde
angieramirez1 <patataramirez13@gmail.com>
2020-05-12 16:10:24 -0500

    Checkout

--- routes.js
+++ routes.js
@@ -56,6 +56,9 @@ function configureRoutes(app,db){
               res.render('product', context);
             });
           });
+          app.get('/checkout', function(req,res){
+            res.render('checkout');
+        });
       
 }
 
